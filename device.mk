acd1c4f975f38a7546fa5ac3d61d37acef14dc6d
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    RMX2185: Build dummy android.hidl.[base,manager]@1.0
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- device.mk
+++ device.mk
@@ -59,6 +59,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/etc/fstab.mt6765:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6765
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
+# Init
+PRODUCT_PACKAGES += \
+    fstab.mt6765
+
 # Hotword Enrollement
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
