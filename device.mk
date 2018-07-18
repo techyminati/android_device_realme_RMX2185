fa68a77f0d59ac172010bf4f868130af7e0639d5
Danny Baumann <dannybaumann@web.de>
2018-07-18 17:25:32 +0200

    RMX2185: Enable call recording
    Change-Id: I1b4478b330b13cf5d0fe1ed0d40f43e4b3825f33
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- device.mk
+++ device.mk
@@ -32,6 +32,11 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+	$(LOCAL_PATH)/overlay \
+	$(LOCAL_PATH)/overlay-lineage
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
