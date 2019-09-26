a748a9b860b56a2c2b9aa18799fa8241535bdda1
Erfan Abdi <erfangplus@gmail.com>
2019-09-26 19:40:20 +0330

    RMX2185: Write Fingerprint HAL Adapter * Tho realme C11 Does Not Sports a Fingerprint Sensor But realme C12/15 Does, So Better we Add Support for FP, this does not causes any harm to C11
    Change-Id: Id33db73425bb5e92ef5c45e2f8d40fa462942af3
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- device.mk
+++ device.mk
@@ -68,6 +68,13 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.rmx2020
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
+
 # Init
 PRODUCT_PACKAGES += \
     init.mt6765.rc \
