60c4a6d6186118e2f09748376ca5485f58e3a431
Peter Cai <peter@typeblog.net>
2020-07-27 17:39:33 +0800

    RMX2185: sepolicy: address rild denials for IMS
    * rild needs to be able to read some props for IMS to register.
    * Unfortunately this means that we have to enable a dirty hack: do not
      check neverallows. This allows us to re-define rild domain in system
      and add rules to it.
    * We have to use a macro to exclude this rule from recovery because
      apparently recovery doesn't care about the no-neverallow flag.
    
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,6 +129,10 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 # Sepolicy
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
+# We have to skip checkpolicy because we have to re-define rild
+# in device system sepolicy to work around IMS issues.
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_VNDK_VERSION := current
