62c8a7926ed27836881014e035b32aa3cc7fd6a5
Peter Cai <peter@typeblog.net>
2020-12-11 15:06:09 +0530

    RMX2185: Import IMS blobs
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- device.mk
+++ device.mk
@@ -55,3 +55,13 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Telephony Jars
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
