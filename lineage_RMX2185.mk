dd0e43e87d382a2953410da2c50a57605f6a2301
Aryan Sinha <sinha.aryan03@gmail.com>
2021-01-07 22:58:47 +0530

    RMX2185 : Import Initial Blobs

---
+++ lineage_RMX2185.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2020 Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from wasabi device makefile
+$(call inherit-product, device/realme/RMX2185/device.mk)
+
+# Inherit some common stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_RMX2185
+PRODUCT_DEVICE := RMX2185
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme C11
+PRODUCT_MANUFACTURER := realme
+
+# Build info
+BUILD_FINGERPRINT := "realme/RMX2185/RMX2185L3:10/QP1A.190711.020/1598241000:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=RMX2185 \
+    PRODUCT_NAME=RMX2185 \
+    PRIVATE_BUILD_DESC="full_oppo6765-user 10 QP1A.190711.020 0b36b6c4f9b3bf3b release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
