5106417be6be3cceec338e4de5c1390b6be0494a
techyminati <sinha.aryan03@gmail.com>
2021-02-27 23:30:19 +0530

    RMX2185: Drop common MTK sepolicy rules * Since we Are using Phhusson's IMS , This Aint required
    This reverts commit f1eabb774c2919d4fbd5a166bc5cea908b2299a5.
    
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,16 +139,6 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
 # in device system sepolicy to work around IMS issues.
 SELINUX_IGNORE_NEVERALLOWS := true
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/mediatek/sepolicy/basic/plat_public \
-    device/mediatek/sepolicy/bsp/plat_public \
-    device/mediatek/sepolicy/full/plat_public
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/mediatek/sepolicy/basic/plat_private \
-    device/mediatek/sepolicy/bsp/plat_private \
-    device/mediatek/sepolicy/full/plat_private
-
 # Treble
 BOARD_VNDK_VERSION := current
 
