93db64c7b2f61506b56f4ce1c5be188b8190e6c7
Jerry Zhang <zhangjerry@google.com>
2021-03-11 22:55:26 +0530

    RMX2185: rootdir: Set sys.usb.ffs.aio_compat && Misc Additions The kernel does not support aio with ffs. Set the property so adb/mtp know to use the compatible functions.
    Bug: 37916658
    Test: Use adb / use adb in recovery
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -33,17 +33,9 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.ago.rc
+LOCAL_MODULE       := init.safailnet.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.ago.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.enableswap
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.enableswap
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
+LOCAL_SRC_FILES    := etc/init.safailnet.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
